94ba1c8f91696b910f2e8e28650218d2966b8ef3
corneliusroot <geocrasher@gmail.com>
2016-05-19 15:36:41 -0700

    Create checkrbl.sh
    CheckRBL.sh simply checks the logs to see how effective the RBL's are at blocking inbound mail. It also checks for mails rejected for failing a reverse dns check. This is inspired by PFLogSumm which is a great perl script for doing the exact same thing way more verbosely and thoroughly, but this is in an easier to read format.

---
+++ checkrbl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# For Postfix servers 
+# 
+# Licensed under the MIT License
+# Count how often the RBL's are blocking mail to check for effectiveness
+
+#gonna need a tmpfile too, might as well make it a unique name
+tmpfile="/tmp/`date +%N`.checkrbl"
+
+#get a list of the RBL's
+postfixrbl=$(grep reject_rbl_client /etc/postfix/main.cf | awk '{ print $2 }' | cut -d, -f1)
+
+#define the log file we'll be checking
+mlog=/var/log/maillog
+
+#do the needful
+for rbl in $postfixrbl; do
+		
+	# count the number of times it's grepped. Probably inneficient to do this for every single one, but it works.
+	# assign it to a variable so it's easier to display with one echo statement
+	rblcount=$(grep $rbl $mlog | wc -l)
+
+	#display it with aforementioned echo statement and dump it into a tempfile
+	printf "%-5s%1s\n" $rblcount   $rbl >> $tmpfile
+done
+
+# show the number of items rejected due to bad reverse dns
+	rejectcount=$(grep "cannot find your hostname" /var/log/maillog | wc -l)
+	printf "$rejectcount Messages rejected due to failed rDNS" >> $tmpfile
+sort -n $tmpfile
+echo "--------------------"
+echo `awk '{s+=$1} END {print s}' $tmpfile` Total Spam Messages Blocked
+rm -f $tmpfile
